ff97103b3c2e9b04ad6e785b874d07750e9cff57
JaViKo500 <javikogutierrez64@gmail.com>
2026-03-17 14:53:19 -0500

    chore: add shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Button } from './components/ui/button'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <h1>Hello, world!</h1>
+    <Button>Click me</Button>
   </StrictMode>,
 )
