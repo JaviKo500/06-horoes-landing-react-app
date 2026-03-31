3c671814c7b0fe5a3328d83afc25f749dca4a340
JaViKo500 <javikogutierrez64@gmail.com>
2026-03-30 21:59:11 -0500

    feat: add type response and update image url

--- src/heroes/actions/get-hero-by-page.action.ts
+++ src/heroes/actions/get-hero-by-page.action.ts
@@ -1,8 +1,16 @@
 import { heroApi } from "../api/hero.api"
+import type { HeroResponse } from "../types/get-heroes.response";
 
-export const getHeroByPageAction = async () => {
-  const {data} = await heroApi.get('/');
-  console.log('<--------------- JK Get-hero-by-page.action --------------->');
-  console.log(data);
-  return data;
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+export const getHeroByPageAction = async (): Promise<HeroResponse> => {
+  const {data} = await heroApi.get<HeroResponse>('/');
+  const heroes = data.heroes?.map( ( hero ) => ({
+    ...hero,
+    image: `${BASE_URL}/images/${hero.image}`,
+  })); 
+  return {
+    ...data,
+    heroes,
+  };
 }
