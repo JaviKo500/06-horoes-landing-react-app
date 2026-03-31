d548ce8c230931ca62258b608f4dd25f01eff286
JaViKo500 <javikogutierrez64@gmail.com>
2026-03-30 21:42:59 -0500

    feat: add query client and provider

--- src/HeroesApp.tsx
+++ src/HeroesApp.tsx
@@ -1,10 +1,18 @@
 import { RouterProvider } from "react-router"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+
 import { appRouter } from "./router/app.router"
+
+const queryClient = new QueryClient()
 
 export const HeroesApp = () => {
   return (
     <>
-    <RouterProvider router={appRouter} />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <RouterProvider router={appRouter} />
+      </QueryClientProvider>
     </>
   )
 }
