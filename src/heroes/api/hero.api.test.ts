739d8b45a23c62b4d09d76fc32dafdbda20c93e1
JaViKo500 <javikogutierrez64@gmail.com>
2026-04-11 05:44:24 -0500

    fix: port api hero

--- src/heroes/api/hero.api.test.ts
+++ src/heroes/api/hero.api.test.ts
@@ -6,6 +6,6 @@ const BASE_URL = import.meta.env.VITE_API_URL;
 describe('Hero.api.test', () => {
   test( 'should be configured point to test server', () => {
     expect( heroApi.defaults.baseURL ).toBe(`${BASE_URL}/api/heroes`);
-    expect(BASE_URL).toContain('3001');
+    expect(BASE_URL).toContain('3000');
   });
 }); 
