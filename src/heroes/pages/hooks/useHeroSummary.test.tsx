b19a147c4ed1e9c4c95b2e81bc2a02ae4662505c
JaViKo500 <javikogutierrez64@gmail.com>
2026-04-12 14:00:19 -0500

    test: success data summary and mocked api call action

--- src/heroes/pages/hooks/useHeroSummary.test.tsx
+++ src/heroes/pages/hooks/useHeroSummary.test.tsx
@@ -1,8 +1,16 @@
-import { describe, expect, test } from "vitest";
-import { renderHook } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
+import { renderHook, waitFor } from "@testing-library/react";
 import { useHeroSummary } from "./useHeroSummary";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type {PropsWithChildren} from 'react';
+import { getSummaryAction } from "@/heroes/actions/get-summary.action";
+import type { SummaryHeroResponse } from "@/heroes/types/get-summary.response";
+
+vi.mock( '@/heroes/actions/get-summary.action', () => ({
+  getSummaryAction: vi.fn(),
+}))
+
+const mockGetSummaryAction = vi.mocked( getSummaryAction );
 
 const tanStackCustomProvider = () => {
   const queryClient = new QueryClient(
@@ -23,12 +31,56 @@ const tanStackCustomProvider = () => {
 
 describe('UseHeroSummary.test', () => {
   test( 'should return the initial state (isLoading)', () => {
-    const { result } = renderHook(() => useHeroSummary(), {
+    const { result } = renderHook(() => useHeroSummary()  , {
       wrapper: tanStackCustomProvider(),
     });
     expect( result.current.isLoading ).toBeTruthy();
     expect( result.current.data ).toBeUndefined();
     expect( result.current.error ).toBeFalsy();
+  });
+
+  // test( 'should return success state with data when api call succeeds', async () => {
+  //   const { result } = renderHook(() => useHeroSummary(), {
+  //     wrapper: tanStackCustomProvider(),
+  //   });
+
+  //   await waitFor(() => {
+  //     expect( result.current.isSuccess ).toBeTruthy();
+      
+  //   });
+
+  //   expect( result.current.isLoading ).toBeFalsy();
+  //   expect( result.current.data ).toBeDefined();
+  // });
+
+  test( 'should return success state with data when api call succeeds', async () => {
+    const summaryData = {
+      totalHeroes: 10,
+      strongestHero: {
+        id: '1',
+        name: 'Superman',
+      },
+      smartsHero: {
+        id: '2',
+        name: 'Batman',
+      },
+      heroCount: 18,
+      villainCount: 7,
+    } as SummaryHeroResponse;
+
+    mockGetSummaryAction.mockResolvedValue( summaryData );
+    const { result } = renderHook(() => useHeroSummary(), {
+      wrapper: tanStackCustomProvider(),
+    });
+    await waitFor(() => {
+      expect( result.current.isSuccess ).toBeTruthy();
+    });
+
+    expect( result.current.isLoading ).toBeFalsy();
+    expect( result.current.data ).toBeDefined();
+    expect( result.current.isError ).toBeFalsy();
+    expect( mockGetSummaryAction ).toHaveBeenCalled();
+
   });
 
 });
